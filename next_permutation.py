ffd5fddd5cba842533f0b332a331b08459782623
Atharva Gaikwad <atharvagaikwad125@gmail.com>
2026-07-19 02:37:00 +0530

    Compute the next lexicographic permutation

---
+++ next_permutation.py
@@ -0,0 +1,21 @@
+
+def next_permutation(arr):
+    arr = list(arr)
+    i = len(arr) - 2
+    while i >= 0 and arr[i] >= arr[i+1]:
+        i -= 1
+    if i < 0:
+        return sorted(arr)
+    j = len(arr) - 1
+    while arr[j] <= arr[i]:
+        j -= 1
+    arr[i], arr[j] = arr[j], arr[i]
+    arr[i+1:] = reversed(arr[i+1:])
+    return arr
+
+
+def main():
+    print(next_permutation([1, 2, 3]))
+
+if __name__ == "__main__":
+    main()
