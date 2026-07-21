5e45ede743cf9cb5d92cd1920d0ad03af63e04fb
Atharva Gaikwad <atharvagaikwad125@gmail.com>
2026-07-21 20:41:57 +0530

    Perform basic arithmetic operations

---
+++ simple_calculator.py
@@ -0,0 +1,19 @@
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b if b != 0 else None
+
+
+def main():
+    print(add(5, 3), subtract(5, 3), multiply(5, 3), divide(5, 3))
+
+if __name__ == "__main__":
+    main()
