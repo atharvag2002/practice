97905296f1b973d3d29e365ac7008ab690d43f34
Atharva Gaikwad <atharvagaikwad125@gmail.com>
2026-08-10 22:16:53 +0530

    Compute prime factors of a number

---
+++ prime_factors.py
@@ -0,0 +1,17 @@
+
+def prime_factors(n):
+    factors = []
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    return factors
+
+
+def main():
+    print(prime_factors(56))
+
+if __name__ == "__main__":
+    main()
