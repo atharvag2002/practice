6ce2402cadceb3994ea561efd359e4335cac9e90
Atharva Gaikwad <atharvagaikwad125@gmail.com>
2026-08-28 09:50:37 +0530

    Count words in a sentence

---
+++ sentence_word_count.py
@@ -0,0 +1,10 @@
+
+def sentence_word_count(sentence):
+    return len(sentence.split())
+
+
+def main():
+    print(sentence_word_count('How many words are here'))
+
+if __name__ == "__main__":
+    main()
